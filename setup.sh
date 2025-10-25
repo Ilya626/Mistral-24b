a269477429f9b2544cfde4d4b86e748fe7ced30b
Ilya626 <35026524+Ilya626@users.noreply.github.com>
2025-10-26 00:38:45 +0300

    Improve dependency installation in setup script

--- setup.sh
+++ setup.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 # Этот скрипт устанавливает все необходимые зависимости
-
 echo ">>> Обновление пакетов и установка git-lfs..."
 apt-get update
 apt-get install -y git git-lfs
 
-echo ">>> Установка Python-библиотек..."
-pip install -U "mergekit[hf]" "axolotl[flash-attn,deepspeed]" "huggingface_hub[cli]"
+echo ">>> Установка PyTorch с поддержкой CUDA..."
+pip install --upgrade torch==2.1.2 --index-url https://download.pytorch.org/whl/cu121
+
+echo ">>> Установка mergekit..."
+pip install --upgrade "mergekit[hf]"
+
+echo ">>> Установка Axolotl..."
+pip install --upgrade "axolotl[flash-attn,deepspeed]"
+
+echo ">>> Установка huggingface_hub CLI..."
+pip install --upgrade "huggingface_hub[cli]"
+
+echo ">>> Проверка доступности huggingface-cli..."
+huggingface-cli --version
 
 echo ">>> Инициализация Git LFS..."
 git lfs install
